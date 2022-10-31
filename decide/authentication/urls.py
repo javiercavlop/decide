f0b5d329991615284231ede8f4f74fe92a761c72
josgarmar31 <joselugarciamarin2406@gmail.com>
2022-10-31 18:48:46 +0100

    FEAT : Creation of the login form
    Creation of the necessary files for a first display of the login view

--- decide/authentication/urls.py
+++ decide/authentication/urls.py
@@ -1,5 +1,6 @@
 from django.urls import include, path
 from rest_framework.authtoken.views import obtain_auth_token
+from authentication import views
 
 from .views import GetUserView, LogoutView, RegisterView
 
