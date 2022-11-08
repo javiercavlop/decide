091b0251b9e68fc08bc5eea5a6016865d8d8fd6c
gonmarfer2 <gonmarfer2@alum.us.es>
2022-11-08 23:26:03 +0100

    feat: added CensusGroups
    Remade the model to add groups of censuses to filter with.

--- decide/census/models.py
+++ decide/census/models.py
@@ -1,9 +1,27 @@
 from django.db import models
 
+class CensusGroup(models.Model):
+    name = models.CharField(max_length=256,unique=True)
+    
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
 
 class Census(models.Model):
     voting_id = models.PositiveIntegerField()
     voter_id = models.PositiveIntegerField()
+    group = models.ForeignKey(CensusGroup,
+                                blank=True, null=True,
+                                related_name='censuss',
+                                on_delete=models.SET_NULL)
 
     class Meta:
         unique_together = (('voting_id', 'voter_id'),)
+    
+    def __str__(self):
+        selfstr = "({},{})".format(self.voting_id,self.voter_id)
+        if self.group:
+            selfstr = "({},{},{})".format(self.voting_id,self.voter_id,self.group)
+        return selfstr
