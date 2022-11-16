707e2fd3bf63e90f92b83fcdf7f3729cca8a4410
gonmarfer2 <gonmarfer2@alum.us.es>
2022-11-16 20:45:05 +0100

    fix: Only allowed votings
    Now, only the votings in which a user has a census appear on the list.

--- decide/base/views.py
+++ decide/base/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from voting import models
+from voting import models as voting_models
+from census import models as census_models
 
 def main_page(request):
-    votings = models.Voting.objects.filter(end_date__isnull=True)
-    return render(request,'mainpage.html',{'votings':votings})
+    is_anonymous = request.user.is_anonymous
+    allowed_votings = census_models.Census.objects.filter(voter_id=request.user.pk).values('voting_id')
+    votings = voting_models.Voting.objects.filter(end_date__isnull=True,pk__in=allowed_votings)
+    return render(request,'mainpage.html',{'votings':votings,'is_anonymous':is_anonymous})
