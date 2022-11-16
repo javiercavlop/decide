8c1d5bfe2654807739f35bc8e8c633d538006349
gonmarfer2 <gonmarfer2@alum.us.es>
2022-11-16 20:13:25 +0100

    feat: Main page votings and actions
    Now, you can choose a voting (user), create census or votings or admin the site (admin)

--- decide/base/views.py
+++ decide/base/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from voting import models
 
 def main_page(request):
-    return render(request,'mainpage.html')
+    votings = models.Voting.objects.filter(end_date__isnull=True)
+    return render(request,'mainpage.html',{'votings':votings})
