3a032ac2f6accbb45f20853f450358fa90dcaf3a
anac29 <anacondeuni@gmail.com>
2022-11-18 16:49:58 +0100

    TEST: Positive case find in user table
    Load /dashboard and checks if the first user showed is one of the users created in the set up.

--- decide/dashboard/tests.py
+++ decide/dashboard/tests.py
@@ -1,110 +1,58 @@
-import random
-from django.conf import settings
-from django.contrib.auth.models import User
-from base import mods
 from base.tests import BaseTestCase
-from census.models import Census
-from mixnet.mixcrypt import ElGamal
-from mixnet.mixcrypt import MixCrypt
-from mixnet.models import Auth
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from django.utils import timezone
 from voting.models import Voting, Question, QuestionOption
+
 # Create your tests here.
-class StatisticsDocs(BaseTestCase):
+class DashBoard_test_case(StaticLiveServerTestCase):
+
     def setUp(self):
+        # Load base test functionality for decide
+        self.base = BaseTestCase()
+        self.base.setUp()
+
+        options = webdriver.ChromeOptions()
+        options.headless = False
+        self.driver = webdriver.Chrome(options=options)
+
         super().setUp()
 
     def tearDown(self):
         super().tearDown()
+        self.driver.quit()
 
-    def encrypt_msg(self, msg, v, bits=settings.KEYBITS):
-        pk = v.pub_key
-        p, g, y = (pk.p, pk.g, pk.y)
-        k = MixCrypt(bits=bits)
-        k.k = ElGamal.construct((p, g, y))
-        return k.encrypt(msg)
+        self.base.tearDown()
 
-    def create_voting(self):
+    def test_simpleVisualizer(self):
         q = Question(desc='test question')
         q.save()
-        for i in range(5):
-            opt = QuestionOption(question=q, option='option {}'.format(i+1))
-            opt.save()
         v = Voting(name='test voting', question=q)
         v.save()
-
-        a, _ = Auth.objects.get_or_create(url=settings.BASEURL,
-                                          defaults={'me': True, 'name': 'test auth'})
-        a.save()
-        v.auths.add(a)
-
-        return v
-
-    def create_voters(self, v):
-        for i in range(100):
-            u, _ = User.objects.get_or_create(username='testvoter{}'.format(i))
-            u.is_active = True
-            u.save()
-            c = Census(voter_id=u.id, voting_id=v.id)
-            c.save()
-
-    def get_or_create_user(self, pk):
-        user, _ = User.objects.get_or_create(pk=pk)
-        user.username = 'user{}'.format(pk)
-        user.set_password('qwerty')
-        user.save()
-        return user
-
-    def store_votes(self, v):
-        voters = list(Census.objects.filter(voting_id=v.id))
-        voter = voters.pop()
-
-        clear = {}
-        for opt in v.question.options.all():
-            clear[opt.number] = 0
-            for i in range(random.randint(0, 5)):
-                a, b = self.encrypt_msg(opt.number, v)
-                data = {
-                    'voting': v.id,
-                    'voter': voter.voter_id,
-                    'vote': { 'a': a, 'b': b },
-                }
-                clear[opt.number] += 1
-                user = self.get_or_create_user(voter.voter_id)
-                self.login(user=user.username)
-                voter = voters.pop()
-                mods.post('store', json=data)
-        return clear
-
-    def test_complete_voting(self):
-        '''
-        data = {'name': 'Example'}
-        response = self.client.post('/voting/', data, format='json')
-        self.assertEqual(response.status_code, 401)
-
-        # login with user no admin
-        self.login(user='noadmin')
-        response = mods.post('voting', params=data, response=True)
-        self.assertEqual(response.status_code, 403)
-
-        # login with user admin
-        self.login()
-        response = mods.post('voting', params=data, response=True)
-        self.assertEqual(response.status_code, 400)
-
-        data = {
-            'name': 'Example',
-            'desc': 'Description example',
-            'question': 'I want a ',
-            'question_opt': ['cat', 'dog', 'horse']
-        }
-        '''
+        v.create_pubkey()
+        v.start_date = timezone.now()
+        v.save()
 
 
+        response = self.driver.get(f'{self.live_server_url}/visualizer/{v.pk}/')
+        vState = self.driver.find_element(By.TAG_NAME, "h2").text
+        self.assertTrue(vState, "Votación en curso")
 
-        response = self.client.get('/dashboard/download', format='json')
-        self.assertEqual(response.status_code, 301)
-        print(self.assertEqual(response.status_code, 301))
-        print(response)
-        self.assertEqual(response['Content-Disposition'], 'attachment; filename=record.pdf')
+
+    def test_vote_dashboard_user_positive(self):
+        q = Question(desc='test questcccion')
+        q.save()
+        v = Voting(name='test voting', question=q)
+        v.save()
+        v.create_pubkey()
+        v.start_date = timezone.now()
+        v.save()
+        response = self.driver.get(f'{self.live_server_url}/dashboard')
+        percentages_selector = WebDriverWait(self.driver, timeout=10).until(
+            lambda d: d.find_element(by=By.ID, value="noadmin"))
+        self.assertEqual(percentages_selector.text,"noadmin")
+
 
 
