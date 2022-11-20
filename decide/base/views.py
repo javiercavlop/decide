c44aec757722c949d55cc0ce8697d4066d8e8e62
gonmarfer2 <gonmarfer2@alum.us.es>
2022-11-20 17:47:16 +0100

    feat: Main page blocks
    That templeate changes if user is staff. Also shows visualizable votings.

--- decide/base/views.py
+++ decide/base/views.py
@@ -4,6 +4,18 @@ from census import models as census_models
 
 def main_page(request):
     is_anonymous = request.user.is_anonymous
+    is_staff = request.user.is_staff
     allowed_votings = census_models.Census.objects.filter(voter_id=request.user.pk).values('voting_id')
-    votings = voting_models.Voting.objects.filter(end_date__isnull=True,pk__in=allowed_votings)
-    return render(request,'mainpage.html',{'votings':votings,'is_anonymous':is_anonymous})
+
+    voting_votings = voting_models.Voting.objects.filter(end_date__isnull=True,start_date__isnull=False,pk__in=allowed_votings)
+    visualize_votings = voting_models.Voting.objects.filter(end_date__isnull=False,tally__isnull=False,pk__in=allowed_votings)
+
+    if is_anonymous:
+        return render(request,'register.html')
+    else:
+        return render(request,'mainpage.html',{
+                                                'voting_votings':voting_votings,
+                                                'visualize_votings':visualize_votings,
+                                                'is_anonymous':is_anonymous,
+                                                'is_staff':is_staff
+                                                })
