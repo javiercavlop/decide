f06085a1c5459892dd717f2f3c2e8b1fb3f85fcf
Jaime Moscoso Bernal <jaimesfc@live.com>
2022-11-23 13:49:41 +0100

    FIX: Some minor conflicts fixed Fixed some minor conflicts for merging

--- decide/census/views.py
+++ decide/census/views.py
@@ -55,46 +55,42 @@ class CensusCreate(generics.ListCreateAPIView):
  
 
 
-class CensusImport(generics.ListCreateAPIView):
-    permission_classes = (UserIsStaff,)
+@transaction.atomic
+def import_csv(request):
+    cont=2
+    try: 
+        if request.method == 'POST':
+            census_from_csv=[]
+        
+            myfile = request.FILES['myfile'] 
+            df=pd.read_csv(myfile)
 
-    @transaction.atomic
-    @api_view(['GET','POST'])
-    def import_csv(request):
-        cont=2
-        try: 
-            if request.method == 'POST':
-                census_from_csv=[]
-            
-                myfile = request.FILES['myfile'] 
-                df=pd.read_csv(myfile)
+            for d in df.values:
+                try:
+                    group = None
+                    if not math.isnan(d[2]):
+                        group = CensusGroup.objects.get(id=d[2])
 
-                for d in df.values:
-                    try:
-                        group = None
-                        if not math.isnan(d[2]):
-                            group = CensusGroup.objects.get(id=d[2])
-
-                        census = Census(voting_id=d[0], voter_id=d[1],group=group)
-                        census_from_csv.append(census)
-                        cont+=1
-                    except CensusGroup.DoesNotExist:
-                        messages.error(request, 'The input Census Group does not exist, in row {}'.format(cont-1))
-                        return render(request, "csv.html")
-                cont=0
-                for c in census_from_csv:
-                    try:
-                        cont+=1
-                        c.save()
-                    except IntegrityError:
-                        messages.error(request, 'Error trying to import CSV, in row {}. A census cannot be repeated.'.format(cont))
-                        return render(request,"csv.html")
-                messages.success(request, 'Census Created')
-                return render(request,"csv.html")
-        except:
-            messages.error(request, 'Error in CSV data. There are wrong data in row {}'.format(cont+1)) 
+                    census = Census(voting_id=d[0], voter_id=d[1],group=group)
+                    census_from_csv.append(census)
+                    cont+=1
+                except CensusGroup.DoesNotExist:
+                    messages.error(request, 'The input Census Group does not exist, in row {}'.format(cont-1))
+                    return render(request, "csv.html")
+            cont=0
+            for c in census_from_csv:
+                try:
+                    cont+=1
+                    c.save()
+                except IntegrityError:
+                    messages.error(request, 'Error trying to import CSV, in row {}. A census cannot be repeated.'.format(cont))
+                    return render(request,"csv.html")
+            messages.success(request, 'Census Created')
             return render(request,"csv.html")
+    except:
+        messages.error(request, 'Error in CSV data. There are wrong data in row {}'.format(cont+1)) 
         return render(request,"csv.html")
+    return render(request,"csv.html")
 
 
 
