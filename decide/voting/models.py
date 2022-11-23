4924afd366d2bdae861b56f861157009e868a41f
Jose Carlos <j.carlos.ortiz.gutierrez@gmail.com>
2022-11-23 13:44:10 +0100

    FEAT: Calculate the parity of a Voting

--- decide/voting/models.py
+++ decide/voting/models.py
@@ -5,6 +5,7 @@ from django.dispatch import receiver
 
 from base import mods
 from base.models import Auth, Key
+from postproc.admin import *
 
 
 class Question(models.Model):
@@ -42,6 +43,12 @@ class Voting(models.Model):
     tally = JSONField(blank=True, null=True)
     postproc = JSONField(blank=True, null=True)
 
+    #Estas nuevas tres variables nos servirán para calcular los porcentajes de genero que hay en una votación
+    #Cada variable corresponde con el numero de votos de un genero
+    num_votes_M = models.PositiveIntegerField()
+    num_votes_W = models.PositiveIntegerField()
+    num_votes_O = models.PositiveIntegerField()
+
     def create_pubkey(self):
         if self.pub_key or not self.auths.count():
             return
@@ -62,6 +69,37 @@ class Voting(models.Model):
         votes = mods.get('store', params={'voting_id': self.id}, HTTP_AUTHORIZATION='Token ' + token)
         # anon votes
         return [[i['a'], i['b']] for i in votes]
+    
+    #La función get_userid saca una lista con todos los user_id de los miembros que han participado
+    #en una votación determinada, la cual filtramos por voting_id
+    def get_userid(self, token=''):
+        votes = mods.get('store', params={'voting_id': self.id}, HTTP_AUTHORIZATION='Token ' + token)
+        return [i['voter_id'] for i in votes]
+        
+    
+    def get_paridad(self, userid):
+        
+        #Iniciamos el numero de votos a 0 antes de hacer el tally a la votación
+        num_votes_M = 0
+        num_votes_W = 0
+        num_votes_O = 0
+
+        #all_genders devuelve una lista con todos los Userprofiles donde aparecen [user_id, genre]
+        all_genders = UserProfile.objects.all()
+
+        for i in range(len(userid)):
+            for b in all_genders:
+                if (userid[i] == b.user_id):
+                    if (b.genre == 'M'):
+                        num_votes_M = num_votes_M + 1
+                    elif (b.genre == 'W'):
+                        num_votes_W = num_votes_W + 1
+                    elif(b.genre == 'O'):
+                        num_votes_O = num_votes_O + 1
+
+        return [num_votes_M, num_votes_W, num_votes_O]
+        
+    
 
     def tally_votes(self, token=''):
         '''
@@ -69,6 +107,15 @@ class Voting(models.Model):
         '''
 
         votes = self.get_votes(token)
+
+        #userid llama a la función donde sacamos una lista con todos los user_id que han partidicpado en la votación
+        userid = self.get_userid(token)
+        #Calculamos el numero de votos por genero
+        genre = self.get_paridad(userid)
+        #Y lo devolvemos a la votación
+        self.num_votes_M = genre[0]
+        self.num_votes_W = genre[1]
+        self.num_votes_O = genre[2]
 
         auth = self.auths.first()
         shuffle_url = "/shuffle/{}/".format(self.id)
@@ -94,13 +141,13 @@ class Voting(models.Model):
 
         self.tally = response.json()
         self.save()
-
         self.do_postproc()
 
     def do_postproc(self):
         tally = self.tally
         options = self.question.options.all()
-
+        
+    
         opts = []
         for opt in options:
             if isinstance(tally, list):
@@ -110,7 +157,8 @@ class Voting(models.Model):
             opts.append({
                 'option': opt.option,
                 'number': opt.number,
-                'votes': votes
+                'votes': votes,
+                
             })
 
         data = { 'type': 'IDENTITY', 'options': opts }
@@ -119,5 +167,8 @@ class Voting(models.Model):
         self.postproc = postp
         self.save()
 
+        
+
     def __str__(self):
         return self.name
+        
