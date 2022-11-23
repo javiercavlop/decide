b2d15e54e00e1851ce0154d8eb0c92784b9fbb8f
Jaime Moscoso Bernal <jaimesfc@live.com>
2022-11-23 11:45:23 +0100

    FIX: Import census from JSON function fixed and some files and functions deleted Import census from JSON function fixed an error because is not an API. I have also deleted csv.html and import census from csv function because it's from another feature

--- decide/census/urls.py
+++ decide/census/urls.py
@@ -5,8 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.CensusCreate.as_view(), name='census_create'),
     path('<int:voting_id>/', views.CensusDetail.as_view(), name='census_detail'),
-    path('import_csv/', views.CensusImport.import_csv,name="import_csv"),
-    path('import_json/', views.CensusImport.import_json,name="import_json"),
+    path('import_json/', views.import_json,name="import_json"),
     path('censusgroups/',views.CensusGroupCreate.as_view(), name='census_group_list'),
     path('censusgroups/<int:pk>/',views.CensusGroupDetail.as_view(), name='census_group_detail'),
 ]
