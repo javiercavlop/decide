d363a369d65fd48d8d0132184fbb3520c993f484
martirl11 <reyeslopezmarta2001@gmail.com>
2022-12-13 20:46:55 +0100

    FEAT: Base of Visualizer test Writed the setUp and the TearDown of visualizer test

--- decide/visualizer/tests.py
+++ decide/visualizer/tests.py
@@ -1,3 +1,41 @@
 from django.test import TestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.contrib.auth import get_user_model
+from pathlib import Path
 
-# Create your tests here.
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+from base.tests import BaseTestCase
+from voting.models import Question, Voting
+
+class VisualizerTestCase(StaticLiveServerTestCase):
+
+
+    def setUp(self):
+        self.base = BaseTestCase()
+        self.base.setUp()
+        User = get_user_model()
+        user = User.objects.get(username="admin")
+        user.is_staff = True
+        user.is_admin = True
+        user.is_superuser = True
+        user.save()
+
+        options = webdriver.ChromeOptions()
+        options.headless = False
+        path = Path.cwd()
+        prefs = {"download.default_directory": str(path)}
+        options.add_experimental_option("prefs", prefs)
+        self.driver = webdriver.Chrome(options=options)
+
+        super().setUp()            
+            
+    def tearDown(self):           
+        super().tearDown()
+        self.driver.quit()
+
+        self.base.tearDown()
