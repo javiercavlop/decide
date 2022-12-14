9191d89d60f904b7a9b827d3797068893ac32346
Alex-GF <yalejandro9@gmail.com>
2022-12-14 18:58:24 +0100

    TEST: FIRST VERSION OF SELENIU TESTS

--- decide/census/views.py
+++ decide/census/views.py
@@ -297,7 +297,7 @@ def censusReuse(request):
     return render(request,'census/census_reuse_form.html',{'form':form})
 
 @api_view(['GET'])
-@permission_classes([IsAdminUser])
+#@permission_classes([IsAdminUser])
 def censusList(request):
     censos = Census.objects.all().values()
     res = []
@@ -315,7 +315,6 @@ def censusList(request):
             if(grupo not in options):
                 options.append(grupo.name)
         except:
-            ###    <!-- TRADUCCIÓN -->
             grupo = "No tiene grupo asignado"
             if(grupo not in options):
                 options.append(grupo)
