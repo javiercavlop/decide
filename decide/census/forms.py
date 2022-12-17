4fb3713226564285f0c16c33916662b69dd96599
Enrique Muñoz <enrmp.mail@gmail.com>
2022-12-17 21:09:06 +0100

    FIX: CODACY ERRORS
    PASSING CODACY

--- decide/census/forms.py
+++ decide/census/forms.py
@@ -15,6 +15,15 @@ class CensusGroupingForm(forms.Form):
     )
 
 class CensusForm(forms.Form):
-    voting_id=forms.ChoiceField(widget=forms.Select(attrs={'class': "form-control"}), choices=list((x.id, x.id) for x in Voting.objects.all()), label="Elige votación")
-    voter_name=forms.ChoiceField(widget=forms.Select(attrs={'class': "form-control"}), choices=list((x.id, x.username) for x in User.objects.all()), label="Elige votante")
-    group_name=forms.CharField(widget=forms.TextInput(attrs={'class': "form-control"}),label='Escribe el nombre del grupo', required=False, )
+    voting_id=forms.ChoiceField(
+        widget=forms.Select(attrs={'class': "form-control"}),
+        choices=list((x.id, x.id) for x in Voting.objects.all()),
+        label="Elige votación")
+    voter_name=forms.ChoiceField(
+        widget=forms.Select(attrs={'class': "form-control"}),
+        choices=list((x.id, x.username) for x in User.objects.all()),
+        label="Elige votante")
+    group_name=forms.CharField(
+        widget=forms.TextInput(attrs={'class': "form-control"}),
+        label='Escribe el nombre del grupo',
+        required=False)
