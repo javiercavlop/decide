9c71339fb1f7e15757a070c5b895ca8f3cf26d87
Enrique Muñoz <enrmp.mail@gmail.com>
2022-12-18 00:58:16 +0100

    FIX: CODACY ERRORS
    CODACY ERRORS FIXED

--- decide/census/tests.py
+++ decide/census/tests.py
@@ -5,18 +5,6 @@ from base.tests import BaseTestCase
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.webdriver.common.keys import Keys
 from django.test import TestCase, Client
-from voting.models import Voting, Question, QuestionOption
-from mixnet.models import Auth
-from django.utils import timezone
-from django.conf import settings
-
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
